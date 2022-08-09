788ca9214f9a71c7b13a639d8b0e31135f704263
Sanjeeb Dey <sanjeebdey1@gmail.com>
2022-08-10 03:27:16 +0530

    Added asynchronous task handling while showing current timeline

--- obs_auto.py
+++ obs_auto.py
@@ -7,10 +7,27 @@ import json, sys, os, time, csv
 from flask import Flask,request
 from flask import render_template
 from flask import current_app as app
-
+import pandas
+import socket, threading
+import asyncio
+from threading import Event
 
 app = Flask(__name__)
 app.app_context().push()
+thr=0
+print(thr)
+async def tasks(commands):
+	while True:
+		for i in commands.index:		
+			print(u"Switching to {} in {}".format(commands['scene'][i],commands['duration'][i]))
+			await asyncio.sleep(commands['duration'][i]) 
+			ws.call(requests.SetCurrentTransition(commands['transition'][i]))
+			ws.call(requests.SetCurrentScene(commands['scene'][i]))
+		global event
+		if event.is_set():
+			break
+
+	
 
 @app.route("/", methods=["GET"])
 def home():
@@ -59,10 +76,41 @@ def dashboard():
 			scenes.append(name)
 		return render_template("dashboard.html",scenes=scenes, transitions=transitions)
 	if request.method=="POST":
+		
 		items=request.form.items(multi=True)
+
 		print(items)
-		for item in items:
-			print(item)
+		with open('saved.csv', 'w', encoding='UTF8') as f:
+			writer = csv.writer(f)
+			header = ['scene', 'transition', 'duration']
+			writer.writerow(header)
+			i=1
+			row=[]
+			for item in items:
+				if i%2!=0:
+					row.append(item[0].removesuffix('_transition'))
+					row.append(item[1])
+					i=i+1
+				else:
+					row.append(item[1])
+					writer.writerow(row)
+					row=[]
+					i=i+1
+		df = pandas.read_csv('saved.csv')
+		global thr
+		global _thread
+		global event
+		if thr==1:
+			event.set()
+			_thread.join()
+			_thread = threading.Thread(target=asyncio.run, args=(tasks(df),))
+			_thread.start()
+		else:
+			event = Event()
+			_thread = threading.Thread(target=asyncio.run, args=(tasks(df),))
+			_thread.start()
+		thr=1
+
 		scns = ws.call(requests.GetSceneList())
 		scenes=[]
 		for s in scns.getScenes():
@@ -73,7 +121,7 @@ def dashboard():
 		for t in sitions.getTransitions():
 			name = t['name']
 			transitions.append(name)
-		return render_template("dashboard.html",scenes=scenes, transitions=transitions)
+		return render_template("dashboard.html",scenes=scenes, transitions=transitions, flow=df)
 
 	
 # try:
@@ -94,6 +142,7 @@ def dashboard():
 app.app_context().push()
 if __name__ == '__main__':
 	file_exists = exists("config.py")
+	
 	if file_exists:
 		import config
 		host = config.host
@@ -106,5 +155,5 @@ if __name__ == '__main__':
 		ws.connect()
 	else:
 		connected=False
-	app.run(host='localhost',port=8080, debug=True)
+	app.run(host='0.0.0.0',port=8080, debug=True)
 	
